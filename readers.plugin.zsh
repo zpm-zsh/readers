7ecd93ad7dc081da9173a2e095dc7335584fea50
Grigorii Horos <horosgrisa@gmail.com>
2019-01-01 16:34:55 +0200

    Fixes

--- readers.plugin.zsh
+++ readers.plugin.zsh
@@ -2,157 +2,93 @@
 DEPENDENCES_ARCH+=( curl pygmentize@pygmentize )
 DEPENDENCES_DEBIAN+=( curl pygmentize@python-pygments )
 
-function htmlcat() {
+function _create_file(){
   if [[ -z $1 ]]; then
-    FILE=$(mktemp -t XXXXX.html)
+    FILE=$(mktemp -u -t XXXXX)
     cat > $FILE
   else
-    if [[ $1 == "http:"* ]] || [[ $1 == "https:"* ]] || [[ $1 == "ftp:"* ]]; then
-      FILE=$(mktemp -t XXXXX.html)
+    if [[ $1 == ("http:"|"https:"|"ftp:")* ]]; then
+      FILE=$(mktemp -u -t "XXXXX$(basename $1)")
       curl -L --silent "$1" > $FILE
     else
       FILE="$1"
     fi
   fi
-  `whence pygmentize` -f 256 -g -l html $FILE 
+  echo $FILE
+}
+
+
+function hcat() {
+  FILE=$(_create_file $@)
+  $(whence pygmentize) -g $FILE
+}
+
+
+function htmlcat() {
+  FILE=$(_create_file $@)
+  $(whence pygmentize) -l html $FILE
 }
 alias xmlcat=htmlcat
 
 function csscat() {
-  if [[ -z $1 ]]; then
-    FILE=$(mktemp -t XXXXX.css)
-    cat > $FILE
-  else
-    if [[ $1 == "http:"* ]] || [[ $1 == "https:"* ]] || [[ $1 == "ftp:"* ]]; then
-      FILE=$(mktemp -t XXXXX.css)
-      curl -L --silent "$1" > $FILE
-    else
-      FILE=$1
-    fi
-  fi
-  `whence pygmentize` -f 256 -g -l css $FILE 
+  FILE=$(_create_file $@)
+  $(whence pygmentize) -l css $FILE
 }
 
 function jscat() {
-  if [[ -z $1 ]]; then
-    FILE=$(mktemp -t XXXXX.js)
-    cat > $FILE
-  else
-    if [[ $1 == "http:"* ]] || [[ $1 == "https:"* ]] || [[ $1 == "ftp:"* ]]; then
-      FILE=$(mktemp -t XXXXX.js)
-      curl -L --silent $1 > $FILE
-    else
-      FILE=$1
-    fi
-  fi
-  `whence pygmentize` -f 256 -g -l javascript $FILE
+  FILE=$(_create_file $@)
+  $(whence pygmentize) -l javascript $FILE
 }
 
 function jsoncat() {
-  if [[ -z $1 ]]; then
-    FILE=$(mktemp -t XXXXX.json)
-    cat > $FILE
-  else
-    if [[ $1 == "http:"* ]] || [[ $1 == "https:"* ]] || [[ $1 == "ftp:"* ]]; then
-      FILE=$(mktemp -t XXXXX.json)
-      curl -L --silent $1 > $FILE
-    else
-      FILE=$1
-    fi
-  fi
-  prettyjson $FILE
+  FILE=$(_create_file $@)
+  $(whence pygmentize) -l json $FILE
 }
 
-DEPENDENCES_ARCH+=( astyle )
-DEPENDENCES_DEBIAN+=( astyle )
 function cppcat() {
-  if [[ -z $1 ]]; then
-    FILE=$(mktemp -t XXXXX.cpp)
-    cat > $FILE
-  else
-    if [[ $1 == "http:"* ]] || [[ $1 == "https:"* ]] || [[ $1 == "ftp:"* ]]; then
-      FILE=$(mktemp -t XXXXX.cpp)
-      curl -L --silent $1 > $FILE
-    else
-      FILE=$1
-    fi
-  fi
-  astyle < $FILE | `whence pygmentize` -f 256 -g c_cpp
+  FILE=$(_create_file $@)
+  $(whence pygmentize) -l c_cpp $FILE
 }
 alias javacat=cppcat
 
-function hcat() {
-  if [[ -z $1 ]]; then
-    FILE=$(mktemp -t XXXXX)
-    cat > $FILE
-  else
-    if [[ $1 == "http:"* ]] || [[ $1 == "https:"* ]] || [[ $1 == "ftp:"* ]]; then
-      FILE=$(mktemp -t XXXXX)
-      curl -L --silent $1 > $FILE
-    else
-      FILE=$1
-    fi
-  fi
-  `whence pygmentize` -f 256 -g $FILE
-}
+
 
 DEPENDENCES_NPM+=( msee )
 function mdcat() {
-  if [[ -z $1 ]]; then
-    FILE=$(mktemp -t XXXXX.md)
-    cat > $FILE
-  else
-    if [[ $1 == "http:"* ]] || [[ $1 == "https:"* ]] || [[ $1 == "ftp:"* ]]; then
-      FILE=$(mktemp -t XXXXX.md)
-      curl -L --silent $1 > $FILE
-    else
-      FILE=$1
-    fi
-  fi
+  FILE=$(_create_file $@)
   msee "$FILE"
 }
+
 DEPENDENCES_ARCH+=( gpg@gnupg )
 DEPENDENCES_DEBIAN+=( gpg@gnupg )
 function gpgcat() {
-  if [[ -z $1 ]]; then
-    FILE=$(mktemp -t XXXXX.gpg)
-    cat > $FILE
-  else
-    if [[ $1 == "http:"* ]] || [[ $1 == "https:"* ]] || [[ $1 == "ftp:"* ]]; then
-      FILE=$(mktemp -t XXXXX.gpg)
-      curl -L --silent $1 > $FILE
-    else
-      FILE=$1
-    fi
-  fi
+  FILE=$(_create_file $@)
   gpg --quiet --batch -d $FILE
 }
 
 function pdfcat() {
-  if [[ $1 == "http:"* ]] || [[ $1 == "https:"* ]] || [[ $1 == "ftp:"* ]]; then
-    FILE=$(mktemp -t XXXXX.pdf)
-    curl -L --silent $1 > $FILE
-  else
-    FILE=$1
-  fi
+  FILE=$(_create_file $@)
   pdftotext -eol unix -nopgbrk "$FILE" -
 }
 
 DEPENDENCES_ARCH+=( icat convert@imagemagick )
 DEPENDENCES_DEBIAN+=( icat convert@imagemagick )
 function imgcat() {
-  if [ ! -z "$1" ]; then
-    if [[ $1 == "http:"* ]] || [[ $1 == "https:"* ]] || [[ $1 == "ftp:"* ]]; then
-      FILERAW=$(mktemp -t XXXXX)
-      curl -L --silent $1 > $FILERAW
-    else
-      FILERAW="$1"
-    fi
-    FILESIZE=$(stat -c%s "$FILERAW")
-    FILE=$(mktemp -t XXXXX.png)
-    convert $FILERAW $FILE
-    icat -m 24bit $FILERAW
-  else
+  if [[ -z $1 ]]; then
     echo "Usege: image <path-to-image>"
+    return -1
   fi
+
+  FILE=$(_create_file $@)
+
+  case "$FILE" in
+      *.("jpg"|"jpeg"|"png"|"JPG"|"JPEG"|"PNG"))
+        icat -m 24bit "$FILE"
+      ;;
+      *)
+        NEWFILE=$(mktemp -t XXXXX.png)
+        convert "$FILE" "$NEWFILE"
+        icat -m 24bit "$NEWFILE"
+      ;;
+  esac
 }
